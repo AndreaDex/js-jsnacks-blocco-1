6173f9a561d576d8dbcfee49091c4b661edb2354
AndreaDex <desiderio.andrea@outlook.it>
2021-05-10 11:46:55 +0200

    blocco4 snack1b

--- Blocco4/snack1/main.js
+++ Blocco4/snack1/main.js
@@ -1,4 +1,16 @@
-var oggetto = {
+/* 
+ * 1A:
+Creare un oggetto palla che abbia le seguenti proprietà.
+Nome = palla */
+Peso = 10
+var palla = {
     nome: "palla",
     peso: 10,
 }
+
+/* 
+*1B
+Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla. 
+*/
+palla.peso = parseInt(prompt("Cambia il peso di 'palla'"))
+console.log(palla);
